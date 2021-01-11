7178366b0fcc255fc27e41ed71549a113a46ae76
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-11 22:26:51 +0200

    add linq with string

---
+++ Practice1101/Linq1101/Program.cs
@@ -0,0 +1,48 @@
+﻿using Practice1101.Helper;
+using System;
+
+namespace Linq1101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ 11.01
+
+            //1
+            WorkWithString.ShowNumbers();
+
+            //2
+            WorkWithString.ShowNumbersWhichSplitOnThree();
+
+            //3
+            WorkWithString.ShowLinq();
+
+            //4
+            WorkWithString.ShpwWordsWIthLetterA();
+
+            //5
+            WorkWithString.ShowCountOfLetterA();
+
+            //6
+            WorkWithString.ShowTrueOrFalseIfStringContainABB();
+
+            //7
+            WorkWithString.ShowMostLongerString();
+
+            //8
+            WorkWithString.ShowAverageStringLength();
+
+            //9
+            WorkWithString.ShowReversShortString();
+
+            //10
+            WorkWithString.ShowBoolSTringStartFromAA();
+
+            //11
+            WorkWithString.ShowWordsWichStartFromAA();
+
+            Console.ReadLine();
+        }
+    }
+}
