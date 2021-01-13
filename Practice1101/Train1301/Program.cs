988f2524a95ddc7ff6062fd00563c7b581ef931c
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-13 23:58:42 +0200

    get info about directory

---
+++ Practice1101/Train1301/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Train1301.Helper;
+
+namespace Train1301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> partOfAllPath = new List<string>() { @"C:\" };
+            while (true)
+            {
+                string path = string.Join(@"\", partOfAllPath.Select(x => x));
+                DirectoryInfo currentDirectiry = GetInfo.GetDirectory(path);
+                Console.Write($"{path}\n");
+                
+            }
+        }
+
+        
+    }
+}
