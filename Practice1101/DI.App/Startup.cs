97f3d04704441594a37ee251b3f4abb2cfd1137e
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-14 12:24:18 +0200

    Add Service provider

---
+++ Practice1101/DI.App/Startup.cs
@@ -0,0 +1,28 @@
+﻿using DI.App.Abstractions;
+using DI.App.Abstractions.BLL;
+using DI.App.Models;
+using DI.App.Services;
+using DI.App.Services.PL.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI.App
+{
+    public static class Startup
+    {
+        public static IServiceProvider GetProvider()
+        {
+            var provider = new ServiceCollection()
+                .AddSingleton<IUser, User>()
+                .AddSingleton<IUserStore, UserStore>()
+                .AddSingleton<IDatabaseService, InMemoryDatabaseService>()
+                .AddSingleton<ICommand, AddUserCommand>()
+                .AddSingleton<ICommandProcessor, ICommandProcessor>()
+                .BuildServiceProvider();
+
+            return provider;
+        }
+    }
+}
