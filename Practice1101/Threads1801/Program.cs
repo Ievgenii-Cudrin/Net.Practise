c4d08c8ae9f60339e79019220b96523663af9066
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-19 21:02:57 +0200

    update

--- Practice1101/Threads1801/Program.cs
+++ Practice1101/Threads1801/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Threads1801
 {
@@ -17,20 +18,30 @@ namespace Threads1801
         static void Main(string[] args)
         {
             var sv = Stopwatch.StartNew();
-            Thread thread1 = new Thread(() => CreateMass());
-            Thread thread2 = new Thread(() => CreateNewMass());
-            Thread thread3 = new Thread(() => GetMin());
-            Thread thread4 = new Thread(() => GetAverage());
 
-            thread1.Start();
-            thread2.Start();
-            thread3.Start();
-            thread4.Start();
+            Thread[] threads = new Thread[4];
 
-            thread1.Join();
-            thread2.Join();
-            thread3.Join();
-            thread4.Join();
+            var parallelOptions = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            };
+
+            threads[0] = new Thread(() => CreateMass());
+            threads[1] = new Thread(() => CreateNewMass());
+            threads[2] = new Thread(() => GetMin());
+            threads[3] = new Thread(() => GetAverage());
+
+
+            Parallel.For(0, 4, parallelOptions, (i) =>
+            {
+                threads[i].Start();
+            });
+
+
+            threads[1].Join();
+            threads[2].Join();
+            threads[3].Join();
+            threads[0].Join();
 
             sv.Stop();
 
