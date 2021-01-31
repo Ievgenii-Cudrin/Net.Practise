2f8f1bcede50cce8da1e1b6fc85486855abacdbb
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-31 21:53:41 +0200

    Summary

---
+++ Practice1101/Linq1101/Entities/Article.cs
@@ -0,0 +1,13 @@
+﻿using Linq1101.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq1101.Entities
+{
+    public class Article : ArtObject
+    {
+        public int Pages { get; set; }
+    }
+
+}
