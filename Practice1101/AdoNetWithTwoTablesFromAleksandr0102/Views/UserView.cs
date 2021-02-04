bf851704cecf067d346cd8bcee9e0f7410d8fe1a
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-04 18:06:39 +0200

    create users View

--- Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Views/UserView.cs
+++ Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Views/UserView.cs
@@ -1,17 +1,53 @@
-﻿using System;
+﻿using AdoNetWithTwoTablesFromAleksandr0102.DI;
+using AdoNetWithTwoTablesFromAleksandr0102.Entities;
+using AdoNetWithTwoTablesFromAleksandr0102.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdoNetWithTwoTablesFromAleksandr0102.Views
 {
     public class UserView
     {
-        public void ShowAllRole()
+        IUserService userService = Startup.ConfigureService().GetRequiredService<IUserService>();
+        IRoleService roleService = Startup.ConfigureService().GetRequiredService<IRoleService>();
+        public UserView(IUserService userService)
         {
-            foreach (var user in userRepository.GetAll().ToList())
+            this.userService = userService;
+        }
+
+        public void ShowAllUsers()
+        {
+            foreach (var user in userService.GetAllUsers())
             {
-                Console.WriteLine($"{user.Id}. {user.Name}");
+                Console.WriteLine($"{user.Id}. {user.Name}, {user.UserRole.Name}");
             }
+        }
+
+        public void CreateUser()
+        {
+            Console.WriteLine("Enter user name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Available position: ");
+            List<Role> roles = roleService.GetAllRoles().ToList();
+
+            foreach (var role in roles)
+            {
+                Console.WriteLine($"{role.Id} - {role.Name}");
+            }
+
+            Console.WriteLine("Enter position id:");
+            int roleId = Convert.ToInt32(Console.ReadLine());
+
+            User user = new User()
+            {
+                Name = name,
+                UserRole = roles.Where(x => x.Id == roleId).FirstOrDefault()
+            };
+
+            userService.CreateUser(user);
         }
     }
 }
