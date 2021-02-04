b1aa2ce4af34a2d9725ef53f551a454ee05cc9b4
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-04 15:07:58 +0200

    Add Interfaces

---
+++ Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Interfaces/IRoleRepository.cs
@@ -0,0 +1,18 @@
+﻿using AdoNetWithTwoTablesFromAleksandr0102.Entities;
+using System.Collections.Generic;
+
+namespace AdoNetWithTwoTablesFromAleksandr0102.Interfaces
+{
+    public interface IRoleRepository
+    {
+        public IEnumerable<Role> GetAll();
+
+        public Role Get(int id);
+
+        public void Create(Role item);
+
+        public void Update(Role item);
+
+        public void Delete(int id);
+    }
+}
