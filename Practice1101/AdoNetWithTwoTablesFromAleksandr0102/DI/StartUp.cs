7f071f2dbf0bec69ad704ec8d51c3e018a2ac664
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-04 16:13:44 +0200

    add dependency injection

---
+++ Practice1101/AdoNetWithTwoTablesFromAleksandr0102/DI/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using AdoNetWithTwoTablesFromAleksandr0102.Interfaces;
+using AdoNetWithTwoTablesFromAleksandr0102.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoNetWithTwoTablesFromAleksandr0102.DI
+{
+    public static class Startup
+    {
+        public static IServiceProvider ConfigureService()
+        {
+            var provider = new ServiceCollection()
+                .AddTransient<IUserRepository, UserRepository>()
+                .AddTransient<IRoleRepository, RoleRepository>()
+                .BuildServiceProvider();
+
+            return provider;
+        }
+    }
+}
