4394ed82b9f274fbd75a5ec0ed8107fc3596ada8
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-04 14:57:34 +0200

    create application and entitites

---
+++ Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoNetWithTwoTablesFromAleksandr0102.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoNetWithTwoTablesFromAleksandr0102.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int RoleId { get; set; }
+    }
+}
