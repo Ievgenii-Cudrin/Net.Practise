eec2e678fe5340067e00b7f56fd6646212316712
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-04 16:56:42 +0200

    Create user service

---
+++ Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using AdoNetWithTwoTablesFromAleksandr0102.Entities;
+using AdoNetWithTwoTablesFromAleksandr0102.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdoNetWithTwoTablesFromAleksandr0102.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public void CreateRole(User user)
+        {
+            userRepository.Create(user);
+        }
+
+        public void ShowAllRole()
+        {
+            foreach (var user in userRepository.GetAll().ToList())
+            {
+                Console.WriteLine($"{user.Id}. {user.Name}");
+            }
+        }
+
+        public List<Role> GetAllRoles()
+        {
+            return userRepository.GetAll().ToList();
+        }
+
+        public void UpdateRole(Role role)
+        {
+            userRepository.Update(role);
+        }
+
+        public void DeleteRole(int id)
+        {
+            userRepository.Delete(id);
+        }
+    }
+}
