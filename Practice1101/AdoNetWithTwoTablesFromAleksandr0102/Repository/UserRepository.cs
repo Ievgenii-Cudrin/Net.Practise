b45571d7ebb5c0f9a07c9c785dd70df588f95309
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-04 16:56:21 +0200

    Update user repository

--- Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Repository/UserRepository.cs
+++ Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Repository/UserRepository.cs
@@ -13,7 +13,7 @@ namespace AdoNetWithTwoTablesFromAleksandr0102.Repository
         static string connectionString = ConfigurationManager.ConnectionStrings["UserDBConnection"].ConnectionString;
         public void Create(User user)
         {
-            string sqlExpression = String.Format("INSERT INTO Users (Name, RoleId) VALUES (@name, @role)");
+            string sqlExpression = String.Format("INSERT INTO Users (Name, RoleId) VALUES (@name, @roleId)");
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -21,7 +21,7 @@ namespace AdoNetWithTwoTablesFromAleksandr0102.Repository
                 SqlCommand command = new SqlCommand(sqlExpression, connection);
                 SqlParameter nameParam = new SqlParameter("@name", user.Name);
                 command.Parameters.Add(nameParam);
-                SqlParameter roleParam = new SqlParameter("@role", user.RoleId);
+                SqlParameter roleParam = new SqlParameter("@roleId", user.UserRole.Id);
                 command.Parameters.Add(roleParam);
 
                 command.ExecuteNonQuery();
@@ -48,17 +48,23 @@ namespace AdoNetWithTwoTablesFromAleksandr0102.Repository
         public User Get(int id)
         {
             User user = new User();
-            string sqlExpression = "SELECT * FROM Users WHERE Id=@id";
+            string sqlExpression = @"select u.[Id], u.[Name], r.[Id], r.[Name] 
+                                    from [Users] u
+                                    Left Join Roles r On u.[RolesId] = r.[Id]
+                                    Where u.[Id] = @id";
+
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlParameter idParam = new SqlParameter("@id", id);
+                command.Parameters.Add(idParam);
                 SqlDataReader reader = command.ExecuteReader();
 
                 user.Id = reader.GetInt32(0);
                 user.Name = reader.GetString(1);
-                user.RoleId = reader.GetInt32(2);
-
+                user.UserRole.Id = reader.GetInt32(2);
+                user.UserRole.Name = reader.GetString(3);
                 reader.Close();
             }
 
@@ -68,7 +74,9 @@ namespace AdoNetWithTwoTablesFromAleksandr0102.Repository
         public IEnumerable<User> GetAll()
         {
             List<User> users = new List<User>();
-            string sqlExpression = "SELECT * FROM Users";
+            string sqlExpression = @"select u.[Id], u.[Name], r.[Id], r.[Name] 
+                                    from [Users] u
+                                    Left Join Roles r On u.[RolesId] = r.[Id]";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -79,11 +87,16 @@ namespace AdoNetWithTwoTablesFromAleksandr0102.Repository
                 {
                     while (reader.Read())
                     {
+                        Role role = new Role()
+                        {
+                            Id = reader.GetInt32(2),
+                            Name = reader.GetString(3)
+                        };
                         User user = new User()
                         {
                             Id = reader.GetInt32(0),
                             Name = reader.GetString(1),
-                            RoleId = reader.GetInt32(2)
+                            UserRole = role
                         };
 
                         users.Add(user);
@@ -106,7 +119,7 @@ namespace AdoNetWithTwoTablesFromAleksandr0102.Repository
                 SqlCommand command = new SqlCommand(sqlExpression, connection);
                 SqlParameter nameParam = new SqlParameter("@name", user.Name);
                 command.Parameters.Add(nameParam);
-                SqlParameter roleParam = new SqlParameter("@roleId", user.RoleId);
+                SqlParameter roleParam = new SqlParameter("@roleId", user.UserRole.Id);
                 command.Parameters.Add(roleParam);
                 SqlParameter idParam = new SqlParameter("@id", user.Id);
                 command.Parameters.Add(idParam);
