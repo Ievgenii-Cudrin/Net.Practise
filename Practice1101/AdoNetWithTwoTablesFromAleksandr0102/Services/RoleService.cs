21021bc3e09cf1f4be75c3052b9381431c5437d2
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-04 16:13:59 +0200

    create role service

---
+++ Practice1101/AdoNetWithTwoTablesFromAleksandr0102/Services/RoleService.cs
@@ -0,0 +1,47 @@
+﻿using AdoNetWithTwoTablesFromAleksandr0102.Entities;
+using AdoNetWithTwoTablesFromAleksandr0102.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdoNetWithTwoTablesFromAleksandr0102.Services
+{
+    public class RoleService
+    {
+        IRoleRepository roleRepository;
+
+        public RoleService(IRoleRepository roleRepository)
+        {
+            this.roleRepository = roleRepository;
+        }
+
+        public void CreateRole(Role role)
+        {
+            roleRepository.Create(role);
+        }
+
+        public void ShowAllRole()
+        {
+            foreach(var role in roleRepository.GetAll().ToList())
+            {
+                Console.WriteLine($"{role.Id}. {role.Name}");
+            }
+        }
+
+        public List<Role> GetAllRoles()
+        {
+            return roleRepository.GetAll().ToList();
+        }
+
+        public void UpdateRole(Role role)
+        {
+            roleRepository.Update(role);
+        }
+
+        public void DeleteRole(int id)
+        {
+            roleRepository.Delete(id);
+        }
+    }
+}
