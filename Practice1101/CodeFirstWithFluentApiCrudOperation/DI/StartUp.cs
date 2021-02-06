cbe489880d8f694ef8be8a7b786f6d1759fd53ee
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-06 22:42:38 +0200

    add dependency injection

---
+++ Practice1101/CodeFirstWithFluentApiCrudOperation/DI/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using CodeFirstWithFluentApiCrudOperation.DataContext;
+using CodeFirstWithFluentApiCrudOperation.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstWithFluentApiCrudOperation.DI
+{
+    public static class Startup
+    {
+        public static IServiceProvider ConfigureService()
+        {
+            var provider = new ServiceCollection()
+                .AddTransient<IDbContext, ApplicationContext>()
+                .BuildServiceProvider();
+
+            return provider;
+        }
+    }
+}
