18c24eb60cc28ccd45f68ab759df80e702d4a161
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-06 16:57:54 +0200

    Add db first project

---
+++ Practice1101/DBFirstCrudOperation0502/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DBFirstCrudOperation0502.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            EmployeeDepartments = new HashSet<EmployeeDepartment>();
+            EmployeePayHistories = new HashSet<EmployeePayHistory>();
+            JobCandidates = new HashSet<JobCandidate>();
+            SalesPeople = new HashSet<SalesPerson>();
+        }
+
+        public int BusinessEntityId { get; set; }
+        public int NationalIdnumber { get; set; }
+        public int LoginId { get; set; }
+        public int OrganizationLevel { get; set; }
+        public string JobTitle { get; set; }
+        public int PersonId { get; set; }
+
+        public virtual Person Person { get; set; }
+        public virtual ICollection<EmployeeDepartment> EmployeeDepartments { get; set; }
+        public virtual ICollection<EmployeePayHistory> EmployeePayHistories { get; set; }
+        public virtual ICollection<JobCandidate> JobCandidates { get; set; }
+        public virtual ICollection<SalesPerson> SalesPeople { get; set; }
+    }
+}
---
+++ Practice1101/DBFirstCrudOperation0502/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBFirstCrudOperation0502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
