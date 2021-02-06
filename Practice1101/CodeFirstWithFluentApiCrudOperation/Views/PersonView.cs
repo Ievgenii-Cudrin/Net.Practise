5fa3d08efe5a497407074fa4b993f0d2f8a1dbb0
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-07 00:35:36 +0200

    Create PersonView.cs

---
+++ Practice1101/CodeFirstWithFluentApiCrudOperation/Views/PersonView.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirstWithFluentApiCrudOperation.BLLInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstWithFluentApiCrudOperation.Views
+{
+    public class PersonView
+    {
+        IPersonService personService;
+
+        public PersonView(IPersonService personService)
+        {
+            this.personService = personService;
+        }
+    }
+}
