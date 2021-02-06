1fc78c294421aed11b28e222461e4a3d05299d72
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-06 22:25:06 +0200

    add repository interfaces

---
+++ Practice1101/CodeFirstWithFluentApiCrudOperation/Interfaces/IEmployeeRepositoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using CodeFirstWithFluentApiCrudOperation.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstWithFluentApiCrudOperation.Interfaces
+{
+    public interface IEmployeeRepositoryRepository
+    {
+        public IEnumerable<Employee> GetAll();
+
+        public Employee Get(int id);
+
+        public void Create(Employee item);
+
+        public void Update(Employee item);
+
+        public void Delete(int id);
+    }
+}
