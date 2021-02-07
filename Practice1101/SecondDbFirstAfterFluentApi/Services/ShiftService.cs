c9e4f8ebd784a89dc3f2c0a68dfcf49de288b6ca
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-07 19:14:32 +0200

    Add services

---
+++ Practice1101/SecondDbFirstAfterFluentApi/Services/ShiftService.cs
@@ -0,0 +1,44 @@
+﻿using CodeFirstWithFluentApiCrudOperation.BLLInterfaces;
+using CodeFirstWithFluentApiCrudOperation.Interfaces;
+using SecondDbFirstAfterFluentApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeFirstWithFluentApiCrudOperation.Services
+{
+    public class ShiftService : IShiftService
+    {
+        IShiftRepository shiftRepository;
+
+        public ShiftService(IShiftRepository shiftRepository)
+        {
+            this.shiftRepository = shiftRepository;
+        }
+        public void CreateShift(Shift item)
+        {
+            this.shiftRepository.Create(item);
+        }
+
+        public void DeleteShift(int id)
+        {
+            this.shiftRepository.Delete(id);
+        }
+
+        public List<Shift> GetAllShifts()
+        {
+            return this.shiftRepository.GetAll().ToList();
+        }
+
+        public Shift GetShift(int id)
+        {
+            return this.shiftRepository.Get(id);
+        }
+
+        public void UpdateShift(Shift item)
+        {
+            this.shiftRepository.Update(item);
+        }
+    }
+}
