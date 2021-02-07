22d6a022e7b4695881ef0cb5455f2f42f0206536
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-07 12:15:40 +0200

    Update PersonView.cs

--- Practice1101/CodeFirstWithFluentApiCrudOperation/Views/PersonView.cs
+++ Practice1101/CodeFirstWithFluentApiCrudOperation/Views/PersonView.cs
@@ -4,6 +4,7 @@ using CodeFirstWithFluentApiCrudOperation.Entities;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CodeFirstWithFluentApiCrudOperation.Views
@@ -23,6 +24,32 @@ namespace CodeFirstWithFluentApiCrudOperation.Views
 
             personService.CreatePerson(person);
             Console.WriteLine("Person added");
+        }
+
+        public static void UpdatePerson(int id)
+        {
+            Person person = personService.GetPerson(id);
+
+            person.FirstName = "JohnNewName";
+            person.LastName = "LennonNewLatsName";
+            person.PersonType = "EuropeanNewType";
+
+            personService.UpdatePerson(person);
+            Console.WriteLine("Person updated");
+        }
+
+        public static void ShowAllPersons()
+        {
+            foreach(var person in personService.GetAllPerson().ToList())
+            {
+                Console.WriteLine($"{person.PersonID}. {person.FirstName} {person.LastName}");
+            }
+        }
+
+        public static void DeletePerson(int id)
+        {
+            personService.DeletePerson(id);
+            Console.WriteLine("Person deleted");
         }
     }
 }
