0bbf0c1423577f03ba32905d485550f2454e6e21
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-09 23:13:41 +0200

    create user mail service

---
+++ Practice1101/PricticeDapper0802/Interfaces/IUserMailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PricticeDapper0802.Interfaces
+{
+    public interface IUserMailService
+    {
+        public void Add(int userId, int mailId);
+    }
+}
