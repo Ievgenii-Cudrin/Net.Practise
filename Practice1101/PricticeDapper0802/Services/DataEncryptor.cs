c1339fa14a9418706b2cfa72374a3e211b38e2aa
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-10 00:00:44 +0200

    create data encryptor

---
+++ Practice1101/PricticeDapper0802/Services/DataEncryptor.cs
@@ -0,0 +1,19 @@
+﻿using PricticeDapper0802.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PricticeDapper0802.Services
+{
+    public class DataEncryptor : IDataEncryptor
+    {
+        IUserService userService;
+        IMailService mailService;
+
+        public DataEncryptor(IUserService userServ, IMailService mailServ)
+        {
+            this.userService = userServ;
+            this.mailService = mailServ;
+        }
+    }
+}
