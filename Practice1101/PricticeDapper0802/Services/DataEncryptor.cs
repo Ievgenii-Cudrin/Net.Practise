6926c4262b3cfa783447390786c652545e4c497b
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-10 00:20:18 +0200

    create data encryptor

--- Practice1101/PricticeDapper0802/Services/DataEncryptor.cs
+++ Practice1101/PricticeDapper0802/Services/DataEncryptor.cs
@@ -1,6 +1,8 @@
-﻿using PricticeDapper0802.Interfaces;
+﻿using PricticeDapper0802.Entities;
+using PricticeDapper0802.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PricticeDapper0802.Services
@@ -14,6 +16,21 @@ namespace PricticeDapper0802.Services
         {
             this.userService = userServ;
             this.mailService = mailServ;
+        }
+
+        public void EncryptData(string email)
+        {
+            User user = this.userService.GetAllUsers().Where(x => x.Email == email).FirstOrDefault();
+
+            if(user != null)
+            {
+                user.Name = Convert.ToBase64String(Encoding.UTF8.GetBytes(user.Name));
+                user.Surname = Convert.ToBase64String(Encoding.UTF8.GetBytes(user.Surname));
+                user.Email = Convert.ToBase64String(Encoding.UTF8.GetBytes(user.Email));
+                user.DateOfBirth = Convert.ToBase64String(Encoding.UTF8.GetBytes(user.DateOfBirth));
+
+                this.userService.UpdateUser(user);
+            }
         }
     }
 }
