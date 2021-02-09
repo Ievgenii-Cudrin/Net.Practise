bb6fe40096a46a4c04215a6160d11c1c7f23f536
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-09 23:48:36 +0200

    summary

--- Practice1101/PricticeDapper0802/Entities/User.cs
+++ Practice1101/PricticeDapper0802/Entities/User.cs
@@ -12,6 +12,8 @@ namespace PricticeDapper0802.Entities
 
         public string Surname { get; set; }
 
+        public string Email { get; set; }
+
         public DateTime DateOfBirth { get; set; }
     }
 }
---
+++ Practice1101/PricticeDapper0802/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using PricticeDapper0802.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PricticeDapper0802.Interfaces
+{
+    public interface IUserService
+    {
+        public void AddUser(User user);
+
+        public void UpdateUser(User user);
+
+        public void DeleteUser(int id);
+
+        public List<User> GetAllUsers();
+    }
+}
