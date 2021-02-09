b5cdc477d557f9d6ed736986efad5e52fc5860f1
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-09 20:32:09 +0200

    create letter bode class

---
+++ Practice1101/PricticeDapper0802/Entities/LetterBody.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PricticeDapper0802.Entities
+{
+    public class LetterBody
+    {
+        public string To { get; set; }
+
+        public string From { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Letter { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
