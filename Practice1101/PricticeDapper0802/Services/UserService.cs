0ebe259015998b2597105f0f4c45940f0c5c759a
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-09 22:26:30 +0200

    Update UserService.cs

--- Practice1101/PricticeDapper0802/Services/UserService.cs
+++ Practice1101/PricticeDapper0802/Services/UserService.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using PricticeDapper0802.Entities;
+using PricticeDapper0802.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PricticeDapper0802.Services
 {
-    class UserService
+    public class UserService
     {
+        IRepository<User> userRepository;
+        public UserService(IRepository<User> userRepo)
+        {
+            this.userRepository = userRepo;
+        }
+
+        public void AddUser(User user)
+        {
+            this.userRepository.Add(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+            this.userRepository.Update(user);
+        }
+
+        public void DeleteUser(User user)
+        {
+            this.userRepository.Delete(user);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return this.userRepository.FindAll().Result.ToList();
+        }
     }
 }
