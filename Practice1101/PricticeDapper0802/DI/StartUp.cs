84b87eeacdf2104f44e8b412f22a58c829a2c07b
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-09 22:19:57 +0200

    add dep injection

---
+++ Practice1101/PricticeDapper0802/DI/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PricticeDapper0802.Entities;
+using PricticeDapper0802.Interfaces;
+using PricticeDapper0802.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PricticeDapper0802.DI
+{
+    public static class Startup
+    {
+        public static IServiceProvider ConfigureService()
+        {
+            var provider = new ServiceCollection()
+                .AddTransient<IRepository<User>, Repository<User>>()
+                .AddTransient<IRepository<Mail>, Repository<Mail>>()
+                .BuildServiceProvider();
+
+            return provider;
+        }
+    }
+}
