e7c3e7107e0fa96a0bc6ca015022daf233064753
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-12 00:35:34 +0200

    delete bad files

---
+++ Practice1101/Practise1101/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practise1101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Practice1101/Practise1101/SomeBusinessService/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SomeBusinessService.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public int Count { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
