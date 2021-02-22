98f02ec10897600a6e5a7c63ac755a8a283ee11b
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-22 22:10:55 +0200

    add includes method to Task 6

--- Practice1101/JSFromSergei/JSHomeWork/6/script5.js
+++ Practice1101/JSFromSergei/JSHomeWork/6/script5.js
@@ -1,5 +1,7 @@
 ﻿function getCounutOfChars() {
     let str = document.getElementById("number").value;
     var m = str.match(/[aeiou]/gi);
+    const countLetters = Array.from(str).filter(letter => 'aeiou'.includes(letter)).length;
     document.getElementById('info').innerHTML = m.length;
+    document.getElementById('info1').innerHTML = countLetters;
 }
