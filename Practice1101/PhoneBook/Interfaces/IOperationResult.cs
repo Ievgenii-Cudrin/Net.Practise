c3d46e95d8b708a2718807a3364c7bac068118d5
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-03-03 23:00:57 +0200

    create operation result

---
+++ Practice1101/PhoneBook/Interfaces/IOperationResult.cs
@@ -0,0 +1,13 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Interfaces
+{
+    public interface IOperationResult
+    {
+        OperationResult OperationResult { get; set; }
+    }
+}
