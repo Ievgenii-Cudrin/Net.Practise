048fd5f373b752437eb038cba5ee36f254f0bf1d
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-03-03 23:00:39 +0200

    create login service

---
+++ Practice1101/PhoneBook/Interfaces/ILogInService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Interfaces
+{
+    public interface ILogInService
+    {
+        bool LogIn(string name, string password);
+
+        public void LogOut();
+    }
+}
---
+++ Practice1101/PhoneBook/Services/LogInService.cs
@@ -0,0 +1,43 @@
+﻿using PhoneBook.Interfaces;
+using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Services
+{
+    public class LogInService : ILogInService
+    {
+        IRepository<User> userRepository;
+        IWorkWithAuthorizedUser authorizedUser;
+
+        public LogInService(
+            IRepository<User> userRepository,
+            IWorkWithAuthorizedUser authorizedUser)
+        {
+            this.userRepository = userRepository;
+            this.authorizedUser = authorizedUser;
+        }
+
+        public bool LogIn(string name, string password)
+        {
+            User user = this.userRepository.Get(x => x.Name == name && x.Password == password).FirstOrDefault();
+
+            if(user != null)
+            {
+                this.authorizedUser.SetUser(user);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void LogOut()
+        {
+            this.authorizedUser.CleanUser();
+        }
+    }
+}
